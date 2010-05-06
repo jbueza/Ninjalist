c7c8d4f729e8f45f8847e6f2d43ac0c2d3d4d4d3
Jaime Bueza <jbueza@gmail.com>
2010-05-06 01:36:17 -0700

    Fixed up documentation: added debugging/deployment tricks for speedy development and updated TODO list, basically I need to write a batch for Windows developers. Also fixed issues with messaging console.

--- README.md
+++ README.md
@@ -14,8 +14,20 @@ Adds the name of the player with an associated date for tracking purposes.
 
 Scans the current raid group for any known Ninjas.
 
+DEBUGGING AND DEPLOYING TRICKS
+---------------------------------------
+For really fast debugging, you can open up your WoW game, create the following macro: <code>/reload ui</code>
+
+cd into your Ninjalist working copy (not your World of Warcraft Interface Addons directory, this is your deploy directory!)
+
+run ant (build.xml) with the following: <code>ant deploy</code> or simply <code>ant</code>. This will copy your working directory into your Mac OS X World of Warcraft Interface Addons directory.
+
+Once this is complete, you can alt-tab back into your World of Warcraft game client and then type <code>/reload ui</code> or if you made macro for it, hit that.
+
+
 TODO
 ---------------------------------------
+* Create Windows batch (.bat) file for Windows developers that want to contribute to this project
 * Develop a workflow for users to upload their ninjalists to a web service 
   * name of player (ninja)
   * date of the incident
--- options.lua
+++ options.lua
@@ -4,29 +4,25 @@ local LSM = LibStub("LibSharedMedia-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Ninjalist", true)
 LSM:Register("statusbar", "Minimalist", "Interface\\Addons\\Ninjalist\\images\\Minimalist")
 
-
-
-
 local function slashHandler(option)
   -- Mayhaps use Ace to handle slash command? It leaves something to be desired, methinks. -kremonted
   option = string.lower(option)
   
   if option == "add" then
     --Ninjalist:ShowOptions()
-    SendChatMessage("Command: Add ninja", "GUILD")
-  elseif option == "search" then
+    --SendChatMessage("Invoke: AddNinjaCommand(ninjaName)", "GUILD")
+    self:Print("Invoke: AddNinjaCommand(ninjaName)")
+  elseif option == "scan" then
     --self:ToggleFrame(1)
-    SendChatMessage("Command: Search", "GUILD")
+    self:Print("Invoke: Scan(raidGroup)")
+    --SendChatMessage("Invoke: Scan(raidGroup)", "GUILD")
   else
-    SendChatMessage("Valid slash commands are:", "GUILD")
-    
-    SendChatMessage("/ninjalist add name", "GUILD")
-    
-    SendChatMessage("/ninjalist scan", "GUILD")
-    
---    self:Print("Valid slash commands are:")
-  --  self:Print("/ninjalist add name")
-  --  self:Print("/ninjalist scan")
+    --SendChatMessage("Valid slash commands are:", "GUILD")
+    --SendChatMessage("/ninjalist add name", "GUILD")
+    --SendChatMessage("/ninjalist scan", "GUILD")
+    self:Print("Valid slash commands are:")
+    self:Print("/ninjalist add name")
+    self:Print("/ninjalist scan")
   end
 end
 
