5ef9ffe04466989f6ce77d46a095509f24d165fb
Jaime Bueza <jbueza@gmail.com>
2010-05-10 00:01:09 -0700

    Adding cool ninja graphic

--- Ninjalist.lua
+++ Ninjalist.lua
@@ -2,32 +2,39 @@ Ninjalist = LibStub("AceAddon-3.0"):NewAddon("Ninjalist", "AceEvent-3.0", "AceCo
 local L = LibStub("AceLocale-3.0"):GetLocale("Ninjalist", true)
 local ninjas = {}
 
-
-function Ninjalist:initialize() 
-  self:Print("Ninjalist: Initializing Ninjalist")
-  Ninjalist:RegisterEvent("PARTY_MEMBERS_CHANGED","Scan")
-	Ninjalist:RegisterEvent("RAID_ROSTER_UPDATE","Scan")
+function Ninjalist:OnInitialize()
+  -- Assuming the .toc says ## SavedVariables: MyAddonDB
+  self:Print("Ninjalist: Initializing Ninjalist database")
+  self.db = LibStub("AceDB-3.0"):New("NinjalistDB")
+  
+  -- Hook into events
+  self:Print("Ninjalist: Initializing hooks for raid/party changes")
+  self:RegisterEvent("PARTY_MEMBERS_CHANGED","Scan")
+	self:RegisterEvent("RAID_ROSTER_UPDATE","Scan")
 end
+
 
 function Ninjalist:Scan()
   local NumRaidMembers = GetNumRaidMembers()
 	local NumPartyMembers = GetNumPartyMembers()
 	
-	
-	Ninjalist.inGroup = false
-	Ninjalist.inRaid = false
+	self.inGroup = false
+  self.inRaid = false
 
-	if NumPartyMembers > 0 and NumRaidMembers == 0 then Recount.inGroup = true end
-	if NumRaidMembers > 0 then Recount.inRaid = true end
+	if NumPartyMembers > 0 and NumRaidMembers == 0 then Ninjalist.inGroup = true end
+	if NumRaidMembers > 0 then Ninjalist.inRaid = true end
 	
-	SendChatMessage("Ninjalist: scanning for ninjas in current party and raid...", "PARTY")
-  --self:Print("Ninjalist: Finished scanning" .. NumPartyMembers)
-  
-	
+	--SendChatMessage("Ninjalist: scanning for ninjas in current party and raid...", "GUILD")
+	if self.inRaid or self.inGroup then
+	  self:Print("Scanning for ninjas in current party and raid...")
+	end
 end
 
 
-
-
-
-Ninjalist:initialize()
+function Ninjalist:Add(name)
+  if name == "" or name == nil then 
+    return 
+  end
+  self:Print("Added ninja - "..name.." on  "..date("%a %b %d %H:%M:%S %Y"))
+  --SendChatMessage("Ninjalist: Added ninja - "..name.." on  "..date("%a %b %d %H:%M:%S %Y"), "GUILD")
+end
--- options.lua
+++ options.lua
@@ -1,31 +1,63 @@
 local Ninjalist = Ninjalist
-local self = Ninjalist
+local AceGUI = LibStub("AceGUI-3.0")
 local LSM = LibStub("LibSharedMedia-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Ninjalist", true)
 LSM:Register("statusbar", "Minimalist", "Interface\\Addons\\Ninjalist\\images\\Minimalist")
+
+local textStore
+
+function Ninjalist:CreateFrame()
+  --Create the UI
+  self.frame = AceGUI:Create("Frame")
+  self.frame:SetTitle("Ninjalist")
+  self.frame:SetStatusText("Developed by Jaime Bueza")
+  self.frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
+  self.frame:SetLayout("Flow")
+  self.frame:SetHeight(200)
+
+  local editbox = AceGUI:Create("EditBox")
+  editbox:SetLabel("Enter ninja name here:")
+  editbox:SetWidth(200)
+  editbox:SetHeight(50)
+  editbox:SetCallback("OnEnterPressed", function(widget, event, text) textStore = text end)
+  --editbox:HideButton(editbox)
+  self.frame:AddChild(editbox)
+
+  local btnAdd = AceGUI:Create("Button")
+  btnAdd:SetText("Add Ninja")
+  btnAdd:SetWidth(200)
+  btnAdd:SetCallback("OnClick", function() Ninjalist:Add(textStore) end)
+  self.frame:AddChild(btnAdd)
+end
+
+function Ninjalist:Toggle()
+  if self.frame and self.frame:IsShown()  then
+    self.frame:Hide()
+  else
+    if ( not self.frame ) then
+      self:CreateFrame()
+    end
+    self.frame:Show()
+  end
+end
 
 local function slashHandler(option)
   -- Mayhaps use Ace to handle slash command? It leaves something to be desired, methinks. -kremonted
   option = string.lower(option)
   
   if option == "add" then
-    --Ninjalist:ShowOptions()
+
     --SendChatMessage("Invoke: AddNinjaCommand(ninjaName)", "PARTY")
-    self:Print("Invoke: AddNinjaCommand(ninjaName)")
+    Ninjalist:Print("Invoke: AddNinjaCommand(ninjaName)")  
   elseif option == "scan" then
     --self:ToggleFrame(1)
-    self:Print("Invoke: Scan(raidGroup)")
+    Ninjalist:Print("Invoke: Scan(raidGroup)")
     Ninjalist:Scan()
     --SendChatMessage("Invoke: Scan(raidGroup)", "GUILD")
   else
-    --SendChatMessage("Valid slash commands are:", "GUILD")
-    --SendChatMessage("/ninjalist add name", "GUILD")
-    --SendChatMessage("/ninjalist scan", "GUILD")
-    self:Print("Valid slash commands are:")
-    self:Print("/ninjalist add name")
-    self:Print("/ninjalist scan")
+    Ninjalist:Toggle()
   end
 end
 
 
-self:RegisterChatCommand("ninjalist", slashHandler)
+Ninjalist:RegisterChatCommand("ninjalist", slashHandler)
